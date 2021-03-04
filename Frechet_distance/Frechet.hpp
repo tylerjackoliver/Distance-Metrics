6fb8e27207cc5170a7dee67526f09e7efbd495ea
Jack Tyler <tyler.jackoliver@gmail.com>
2021-03-04 21:56:31 +0000

    Update Frechet.hpp

--- Frechet_distance/Frechet.hpp
+++ Frechet_distance/Frechet.hpp
@@ -1,3 +1,19 @@
+/*  Frechet Distance
+    Copyright (C) 2021 J. Tyler
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
 #ifndef __FRECHET_H__
 #define __FRECHET_H__
 
@@ -158,3 +174,4 @@ namespace Frechet
         return frechetMatrix[ l1.size() - 1 ][ l2.size() - 1 ];
     }
 };
+#endif
